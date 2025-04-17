e4a156bb5b724a47beedb0fce81ec639e50abae7
scalvaruso <calvaruso.simone@gmail.com>
2025-04-17 12:27:58 +0100

    Created Class TextLineBreaker

--- textlinebreaker/textlinebreaker.py
+++ textlinebreaker/textlinebreaker.py
@@ -1,113 +1,109 @@
+
+"""
+Text Line Breaker is a Python script that takes a list of text items,
+breaks them into lines of a specified maximum width.
+This can be particularly useful for formatting and displaying text in a terminal.
+
+Parameters:
+    line: is the main argument, it's the text that needs to be broken down
+    max_width: allows to set the max length of text on a line.
+        default value = terminal width
+        allowed values: integers, "min", "2words", "max"
+    alignment: allows to change the alignment of the text inside the frame.
+        default value = "left"
+        allowed values: "left", "centre", "center", "right"
+"""
 import os  # Import the os module for accessing terminal size and other system-related functions
 
-def split_line(line, max_width=os.get_terminal_size().columns, alignment="left"):
-    """
-    Text Line Breaker is a Python script that takes a list of text items,
-    breaks them into lines of a specified maximum width.
-    This can be particularly useful for formatting and displaying text in a terminal.
+def _get_terminal_columns(default=80):
+    try:
+        return os.get_terminal_size().columns
+    except OSError:
+        return default
 
-    Parameters:
-        line: is the main argument, it's the text that needs to be broken down
-        max_width: allows to set the max length of text on a line.
-            default value = terminal width
-            allowed values: integers, "min", "2words", "max"
-        alignment: allows to change the alignment of the text inside the frame.
-            default value = "left"
-            allowed values: "left", "centre", "center", "right"
-    """
+
+class TextLineBreaker:
+    def __init__(self, text, max_width=_get_terminal_columns(), alignment="left"):
+        self.text = text
+        self.max_width = max_width
+        self.alignment = alignment
+        self.formatted_text = self.split_line()
     
-    # Get a list of words from the input line
-    words = _valid_input(line)
+    def split_line(self):
+        words = self._valid_input(self.text)
+        max_width = self._valid_width(self.max_width, words)
+        new_list = []
+        new_line = ""
+        space = " "
     
-    # Validate and adjust the maximum width: at least one word, less than the terminal width.
-    max_width = _valid_width(max_width, words)
+        if self.alignment.lower() == "right":
+            positioning = "{:>"
+        elif self.alignment.lower() in ["centre", "center"]:
+            positioning = "{:^"
+        else:
+            positioning = "{:<"
+        
+        adjustment = positioning + f"{max_width}" + "}"
+        
+        if words == [""]:
+            new_list.append(" " * max_width)
+        
+        for word in words:
+            word = word.replace("\t", "    ")
+
+            if len(new_line + word) < (max_width + 1):
+                new_line += word + space
+            else:
+                if len(word) > max_width:
+                    remaining_width = max_width - len(new_line)
+                    new_line += word[:remaining_width] + space
+                    word = word[remaining_width:]
+                    new_list.append(adjustment.format(new_line.rstrip(" ")))
+
+                    while len(word) > max_width:
+                        new_line = word[:max_width] + space
+                        word = word[max_width:]
+                        new_list.append(adjustment.format(new_line.rstrip(" ")))
+                    new_line = word + space
+                else:
+                    new_list.append(adjustment.format(new_line.rstrip(" ")))
+                    new_line = word + space
+        
+        if new_line != " ":
+            new_list.append(adjustment.format(new_line.rstrip(" ")))
+        
+        return new_list
+
+    def _valid_input(self, line):
+        words = []
+        if isinstance(line, list):
+            for i in line:
+                if isinstance(i, list):
+                    words += i
+                else:
+                    words += i.split(" ")
+        else:
+            words = line.split(" ")
+        return words
     
-    # Create a new list to store the new lines
-    new_list = []  # Initialize an empty list to store lines
-    new_line = ""  # Initialize an empty string to store the current line being constructed
-    space = " "  # Initialize a variable for space character
+    def _valid_width(self, max_width, words):
+        min_width = max(len(word) for word in words)
+        if isinstance(max_width, int):
+            if 9 <= max_width <= os.get_terminal_size().columns:
+                pass
+            elif max_width < min_width:
+                max_width = min_width
+            else:
+                max_width = os.get_terminal_size().columns
+        elif max_width.lower() == "min":
+            max_width = min_width
+        elif max_width.lower() == "2words":
+            max_width = min_width + min(len(word) for word in words)
+        else:
+            max_width = os.get_terminal_size().columns
+        return max_width
     
-    # Adjust the alignment of the lines based on the specified alignment
-    if alignment.lower() == "right":  # Right alignment
-        positioning = "{:>"  # Format specifier for right alignment
-    elif alignment.lower()=="centre" or alignment.lower()=="center":  # Center alignment
-        positioning = "{:^"  # Format specifier for center alignment
-    else:  # Left alignment (default)
-        positioning = "{:<"  # Format specifier for left alignment
-    adjustment = positioning + f"{max_width}" + "}"  # Adjustment string for formatting
-    
-    # Split the lines if they are longer than the max length characters.
-    if words == [""]:  # If there are no words
-        new_list.append(" "*max_width)  # Append an empty line with spaces
-    
-    for word in words:  # Iterate through each word
-        word = word.replace("\t", "    ")  # Replace tab with four spaces
-
-        if len(new_line+word) < (max_width+1):  # If adding the word does not exceed max width
-            new_line += word + space  # Add the word to the current line
-
-        else:  # If adding the word exceeds max width
-            if len(word) > max_width:  # If the word itself is longer than max width
-
-                remaining_width = max_width - len(new_line)  # Calculate remaining space in the line
-                new_line += word[:remaining_width] + space  # Add part of the word to the line
-                word = word[remaining_width:]  # Update word to exclude the part added
-                new_list.append(adjustment.format(new_line.rstrip(" ")))  # Append the line to new_list
-
-                while len(word) > max_width:  # If the remaining part of the word is still longer than max width
-                    new_line = word[:max_width] + space  # Add part of the word to the line
-                    word = word[max_width:]  # Update word to exclude the part added
-                    new_list.append(adjustment.format(new_line.rstrip(" ")))  # Append the line to new_list
-                new_line = word + space  # Update new_line to contain the remaining part of the word
-
-            else:  # If the word itself fits within the max width
-                new_list.append(adjustment.format(new_line.rstrip(" ")))  # Append the current line to new_list
-                new_line = word + space  # Start a new line with the current word
-    
-    if new_line != " ":  # If there is content in the last line
-        new_list.append(adjustment.format(new_line.rstrip(" ")))  # Append the last line to new_list
-    
-    return new_list  # Return the list of formatted lines
+    def __str__(self):
+        return "\n".join(self.formatted_text)
 
 
-def _valid_input(line):
-    # This function validate the input and width
-    # Convert input into a list of words
-
-    words = []  # Initialize an empty list for words
-
-    if isinstance(line, list):  # If input is already a list
-        for i in line:  # Iterate through each item in the list
-            if isinstance(i, list):  # If the item is also a list
-                words += i  # Extend words list with the sublist
-            else:  # If the item is not a list
-                words += i.split(" ")  # Split the item by spaces and extend words list
-    else:  # If input is not a list
-        words = line.split(" ")  # Split the line by spaces
-    
-    return words  # Return the list of words
-
-
-def _valid_width(max_width, words):
-    # Check if the given max width is a valid number
-    # and lower than the terminal width
-
-    min_width = max(len(word) for word in words)  # Get the maximum word length
-    
-    if isinstance(max_width, int):  # If max_width is an integer
-
-        if 9 <= max_width <= os.get_terminal_size().columns:  # If max_width is within valid range.
-            pass  # Do nothing
-        elif max_width < min_width:  # If max_width is less than the minimum word length
-            max_width = min_width  # Set max_width to the minimum word length
-        else:  # If max_width exceeds terminal width
-            max_width = os.get_terminal_size().columns  # Set max_width to terminal width
-
-    elif max_width.lower()=="min":  # If max_width is set to "min"
-        max_width = min_width  # Set max_width to the minimum word length
-    elif max_width.lower()=="2words":  # If max_width is set to "2words"
-        max_width = min_width + min(len(word) for word in words)  # Set max_width to the sum of two minimum word lengths
-    else:  # If max_width is not an integer or a recognized keyword
-        max_width = os.get_terminal_size().columns  # Set max_width to terminal width
-    
-    return max_width  # Return the validated max width
